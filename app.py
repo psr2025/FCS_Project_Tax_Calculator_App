0e14ac2e849bae489c50984ebe3d35c31cd02a12
psr2025 <philippraupp@outlook.com>
2025-12-04 00:45:05 +0100

    update

--- app.py
+++ app.py
@@ -309,23 +309,17 @@ def calculation_total_income_tax(
     # federal tax
     federal_tax = calculation_income_tax_federal(tax_rates_federal, marital_status=marital_status, number_of_children=number_of_children, income_net=income_net)
 
-    # Base cantonal tax (before multipliers)
+    # base cantonal tax (before multipliers)
     base_income_tax_cantonal = calculation_income_tax_base_SG(tax_rates_cantonal, income_net)
 
-    # Cantonal + municipal + church tax (after multipliers)
-    (
-        total_cant_muni_church,
-        tax_canton,
-        tax_commune,
-        tax_church,
-    ) = calculation_cantonal_municipal_church_tax(
+    # cantonal + municipal + church tax (after multipliers)
+    (total_canton_municipal_church, tax_canton, tax_commune, tax_church) = calculation_cantonal_municipal_church_tax(
         tax_multiplicators_cantonal_municipal,
         base_income_tax_cantonal,
         commune,
-        church_affiliation,
-    )
+        church_affiliation)
 
-    total_tax = federal_tax + total_cant_muni_church
+    total_income_tax = federal_tax + total_canton_municipal_church
 
     return {
         "federal_tax": federal_tax,
@@ -333,8 +327,8 @@ def calculation_total_income_tax(
         "cantonal_tax": tax_canton,
         "municipal_tax": tax_commune,
         "church_tax": tax_church,
-        "total_cantonal_municipal_church_tax": total_cant_muni_church,
-        "total_tax": total_tax,
+        "total_cantonal_municipal_church_tax": total_canton_municipal_church,
+        "total_income_tax": total_income_tax,
     }
 
 
