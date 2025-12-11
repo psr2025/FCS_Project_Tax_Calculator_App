fd66e0fdfeef5699e735c9fccd12817c99966ad7
psr2025 <philippraupp@outlook.com>
2025-12-11 19:13:29 +0100

    deleted old files

--- tax_calculator_app/data/constants.py
+++ tax_calculator_app/data/constants.py
@@ -1,9 +1,9 @@
 # data/constants.py
-#
+
 # Defining social deduction rates for employed and self-empleyed users as a share of gross income.
 # AHV - Old-Age & Survivors Insurance
 ahv_rate_employed = 0.0435
-ahv_rate_self_employed = 0.081 #### We assume that the self employed user earns >= CHF 60'500 -- eventually remove this restriction with this table "2025_social_contribution_tables_self_employment"
+ahv_rate_self_employed = 0.081 
 
 # IV - Disability Insurance
 iv_rate_employed = 0.007
